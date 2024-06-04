65338c6705c3e578471a20a8e43c6f757f7421d3
andrewwwmatsko <matskoandrew@gmail.com>
2024-06-04 14:46:27 +0300

    aos animation for the sections

--- src/main.js
+++ src/main.js
@@ -4,3 +4,9 @@ import Accordion from 'accordion-js';
 new Accordion('.accordion-container', {
   openOnInit: [0],
 });
+
+//aos
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
